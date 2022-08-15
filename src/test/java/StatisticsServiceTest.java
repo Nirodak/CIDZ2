5731f4684ebf8dec97ee00198aa60b5920e7c176
Nikolay Rodak <rodak1993@gmail.com>
2022-08-15 20:39:19 +0300

    bag try

---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,31 @@
+import ru.netology.statistic.StatisticsService;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIfUpper2 (){
+        StatisticsService service = new StatisticsService();
+
+        long[]  incomesInBillions = {15,20,10,8,7,6,5};
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+}
